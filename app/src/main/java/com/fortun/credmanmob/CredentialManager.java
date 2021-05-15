4842d96b03123554b38978e987bf56a0b867574a
JFortun <josemanuelfortunromero@gmail.com>
2021-05-15 13:29:47 +0200

    Implemented basic Credential Manager

--- app/src/main/java/com/fortun/credmanmob/CredentialManager.java
+++ app/src/main/java/com/fortun/credmanmob/CredentialManager.java
@@ -1,7 +1,10 @@
 package com.fortun.credmanmob;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,6 +13,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.fortun.credmanmob.httpClient.HTTPClientCredentials;
 import com.fortun.credmanmob.httpClient.HTTPClientUsers;
 
 import java.util.ArrayList;
@@ -17,10 +21,10 @@ import java.util.ArrayList;
 public class CredentialManager extends AppCompatActivity implements View.OnClickListener {
 
     ListView credentialsList;
-    ArrayList<String> credentials;
+    ArrayList<String> credentials = new ArrayList<>();
     ArrayAdapter<String> adapter;
-    EditText txtUserManagerUser, txtUserManagerPassword, txtUserManagerPasswordAgain;
-    Button btnSignOut, btnManageUser, btnUserManagerUpdate, btnUserManagerDelete, btnUserManagerCancel;
+    EditText txtUserManagerUser, txtUserManagerPassword, txtUserManagerPasswordAgain, txtCredentialManagerName, txtCredentialManagerUser, txtCredentialManagerPassword;
+    Button btnSignOut, btnManageUser, btnUserManagerUpdate, btnUserManagerDelete, btnUserManagerCancel, btnCredentialManagerNew, btnCredentialManagerCreate, btnCredentialManagerUpdate, btnCredentialManagerCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,37 +35,89 @@ public class CredentialManager extends AppCompatActivity implements View.OnClick
         txtUserManagerUser = findViewById(R.id.txtUserManagerUser);
         txtUserManagerPassword = findViewById(R.id.txtUserManagerPassword);
         txtUserManagerPasswordAgain = findViewById(R.id.txtUserManagerPasswordAgain);
+        txtCredentialManagerName = findViewById(R.id.txtCredentialManagerName);
+        txtCredentialManagerUser = findViewById(R.id.txtCredentialManagerUser);
+        txtCredentialManagerPassword = findViewById(R.id.txtCredentialManagerPassword);
         btnSignOut = findViewById(R.id.btnSignOut);
         btnManageUser = findViewById(R.id.btnManageUser);
         btnUserManagerUpdate = findViewById(R.id.btnUserManagerUpdate);
         btnUserManagerDelete = findViewById(R.id.btnUserManagerDelete);
         btnUserManagerCancel = findViewById(R.id.btnUserManagerCancel);
+        btnCredentialManagerNew = findViewById(R.id.btnCredentialManagerNew);
+        btnCredentialManagerCreate = findViewById(R.id.btnCredentialManagerCreate);
+        btnCredentialManagerUpdate = findViewById(R.id.btnCredentialManagerUpdate);
+        btnCredentialManagerCancel = findViewById(R.id.btnCredentialManagerCancel);
 
         btnSignOut.setOnClickListener(v -> finish());
         btnManageUser.setOnClickListener(this);
         btnUserManagerUpdate.setOnClickListener(this);
         btnUserManagerDelete.setOnClickListener(this);
         btnUserManagerCancel.setOnClickListener(this);
+        btnCredentialManagerNew.setOnClickListener(this);
+        btnCredentialManagerCreate.setOnClickListener(this);
+        btnCredentialManagerUpdate.setOnClickListener(this);
+        btnCredentialManagerCancel.setOnClickListener(this);
 
         txtUserManagerUser.setVisibility(View.INVISIBLE);
         txtUserManagerPassword.setVisibility(View.INVISIBLE);
         txtUserManagerPasswordAgain.setVisibility(View.INVISIBLE);
+        txtCredentialManagerName.setVisibility(View.INVISIBLE);
+        txtCredentialManagerUser.setVisibility(View.INVISIBLE);
+        txtCredentialManagerPassword.setVisibility(View.INVISIBLE);
         btnUserManagerUpdate.setVisibility(View.INVISIBLE);
         btnUserManagerDelete.setVisibility(View.INVISIBLE);
         btnUserManagerCancel.setVisibility(View.INVISIBLE);
+        btnCredentialManagerCreate.setVisibility(View.INVISIBLE);
+        btnCredentialManagerUpdate.setVisibility(View.INVISIBLE);
+        btnCredentialManagerCancel.setVisibility(View.INVISIBLE);
 
-        credentials = new ArrayList<>();
-        credentials.add("ID     -     NAME     -     USER     -     PASSWORD");
-        credentials.add("ID     -     NAME     -     USER     -     PASSWORD");
-        credentials.add("ID     -     NAME     -     USER     -     PASSWORD");
-        credentials.add("ID     -     NAME     -     USER     -     PASSWORD");
-        credentials.add("ID     -     NAME     -     USER     -     PASSWORD");
+        credentials = HTTPClientCredentials.read("findAll", "null");
 
-        // Crear adaptador
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, credentials);
 
-        // Asignar el adaptador a nuestro ListView
         credentialsList.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+        credentialsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                btnCredentialManagerNew.setVisibility(View.INVISIBLE);
+                btnCredentialManagerUpdate.setVisibility(View.VISIBLE);
+                btnCredentialManagerCancel.setVisibility(View.VISIBLE);
+                txtCredentialManagerName.setVisibility(View.VISIBLE);
+                txtCredentialManagerUser.setVisibility(View.VISIBLE);
+                txtCredentialManagerPassword.setVisibility(View.VISIBLE);
+
+                String[] credentialSplit = credentialsList.getItemAtPosition(position).toString().split("     ");
+                txtCredentialManagerName.setText(credentialSplit[1]);
+                txtCredentialManagerUser.setText(credentialSplit[2]);
+                txtCredentialManagerPassword.setText(credentialSplit[3]);
+            }
+        });
+
+        credentialsList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(CredentialManager.this);
+                builder.setMessage("Are you sure you want to delete this credential? ")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                String[] credentialSplit = credentialsList.getItemAtPosition(position).toString().split("     ");
+                                Toast.makeText(getApplicationContext(), "DELETED", Toast.LENGTH_SHORT).show();
+
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert = builder.create();
+                alert.show();
+
+                return true;
+            }
+        });
 
     }
 
@@ -112,6 +168,39 @@ public class CredentialManager extends AppCompatActivity implements View.OnClick
             btnUserManagerDelete.setVisibility(View.INVISIBLE);
             btnUserManagerCancel.setVisibility(View.INVISIBLE);
 
+        } else if (v.getId() == R.id.btnCredentialManagerNew) {
+            btnCredentialManagerNew.setVisibility(View.INVISIBLE);
+            btnCredentialManagerCreate.setVisibility(View.VISIBLE);
+            btnCredentialManagerCancel.setVisibility(View.VISIBLE);
+            txtCredentialManagerName.setVisibility(View.VISIBLE);
+            txtCredentialManagerUser.setVisibility(View.VISIBLE);
+            txtCredentialManagerPassword.setVisibility(View.VISIBLE);
+        } else if (v.getId() == R.id.btnCredentialManagerCreate) {
+            Toast.makeText(this, "Credential created", Toast.LENGTH_SHORT).show();
+            btnCredentialManagerNew.setVisibility(View.VISIBLE);
+            btnCredentialManagerCreate.setVisibility(View.INVISIBLE);
+            btnCredentialManagerUpdate.setVisibility(View.INVISIBLE);
+            btnCredentialManagerCancel.setVisibility(View.INVISIBLE);
+            txtCredentialManagerName.setVisibility(View.INVISIBLE);
+            txtCredentialManagerUser.setVisibility(View.INVISIBLE);
+            txtCredentialManagerPassword.setVisibility(View.INVISIBLE);
+        } else if (v.getId() == R.id.btnCredentialManagerUpdate) {
+            Toast.makeText(this, "Credential updated", Toast.LENGTH_SHORT).show();
+            btnCredentialManagerNew.setVisibility(View.VISIBLE);
+            btnCredentialManagerCreate.setVisibility(View.INVISIBLE);
+            btnCredentialManagerUpdate.setVisibility(View.INVISIBLE);
+            btnCredentialManagerCancel.setVisibility(View.INVISIBLE);
+            txtCredentialManagerName.setVisibility(View.INVISIBLE);
+            txtCredentialManagerUser.setVisibility(View.INVISIBLE);
+            txtCredentialManagerPassword.setVisibility(View.INVISIBLE);
+        } else if (v.getId() == R.id.btnCredentialManagerCancel) {
+            btnCredentialManagerNew.setVisibility(View.VISIBLE);
+            btnCredentialManagerCreate.setVisibility(View.INVISIBLE);
+            btnCredentialManagerUpdate.setVisibility(View.INVISIBLE);
+            btnCredentialManagerCancel.setVisibility(View.INVISIBLE);
+            txtCredentialManagerName.setVisibility(View.INVISIBLE);
+            txtCredentialManagerUser.setVisibility(View.INVISIBLE);
+            txtCredentialManagerPassword.setVisibility(View.INVISIBLE);
         }
     }
 }
